f90a32eca35ea2880835b01a663a415a69b8e195
Mohak157 <girimohak73@gmail.com>
2026-02-10 19:07:34 +0545

    added new newode function

--- Browser_history_linkedlist.c
+++ Browser_history_linkedlist.c
@@ -7,7 +7,7 @@ struct Page{ //Doubly linked list
     struct Page *next;
     struct Page *prev;
 };
-
+struct Page *start =NULL;
 void loadingbar(){ // loading bar effect using sleep()
     printf("Loading ::");
     for (int i=0;i<5;i++){
@@ -17,7 +17,34 @@ void loadingbar(){ // loading bar effect using sleep()
     printf("\n");
 }
 
+struct Page *add_history(char *url){  //Creating a new node
+    struct Page *newpage =(struct Page*)malloc(sizeof(struct Page));
+    strcpy(newpage->url,url); // cant assign string directly
+    newpage->next=NULL;
+    newpage->prev = NULL;
+    return newpage;
+
+}
+
+void page_visit(){
+    char url[50];
+    printf("Enter the url of the site ur visiting:");
+    scanf("%c",&url);
+    loading();
+    struct Page *newpage =add_history(url);
+    if(start==NULL){
+        start=newpage;
+        newpage->next=NULL;
+        newpage->prev=NULL;
+
+    }
+    newpage->next=start;
+    newpage->prev=NULL;
+    start->prev=newpage;
+    start=newpage;
+}
+
 
 int main(){
-    loadingbar();
+    
 }
